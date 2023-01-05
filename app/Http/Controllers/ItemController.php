bea31b26710cb65d30cba2af0b1eb846b5f6d226
DuyManh1802 <105428940+DuyManh1802@users.noreply.github.com>
2023-01-05 13:24:56 +0700

    commit

--- app/Http/Controllers/ItemController.php
+++ app/Http/Controllers/ItemController.php
@@ -1,60 +1,61 @@
 <?php
 
-namespace App\Http\Controllers;
+    namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
-use App\Models\item;
-use App\Models\category;
+    use Illuminate\Http\Request;
+    use App\Models\item;
+    use App\Models\category;
 
-class ItemController extends Controller
-{
-    public function index()
+    class ItemController extends Controller
     {
-        $items = item::all();
-        return view('item.list', compact('items'));
+        public function index()
+        {
+            $items = item::all();
+            return view('item.list', compact('items'));
+        }
+
+        public function create()
+        {
+            $categories = category::all();
+            return view('item.create', compact('categories'));
+        }
+
+        public function store(Request $request)
+        {
+            $this->validate($request, [
+                'item_name' =>'required',
+                'category_id' =>'required',
+            ]);
+
+            item::create([
+                'item_name' =>$request->item_name,
+                'category_id'=>$request->category_id,
+            ]);
+            return redirect()->route('item.index')->with('success', 'Created successfully!' );
+        }
+
+        public function edit($id)
+        {
+            $items = item::find($id);
+            return view('item.edit', compact('items'));
+        }
+
+        public function update(Request $request, $id)
+        {
+            $this->validate($request, [
+                'item_name' =>'required'
+            ]);
+
+            item::where('id', $id)->update([
+                'item_name' =>$request->item_name,
+            ]);
+            return redirect()->route('item.index', $id)->with('success', 'Edited successfully!' );
+        }
+
+        public function delete($id)
+        {
+            item::where('id', $id)->delete();
+            return redirect()->route('item.index')->with('success', 'Deleted successfully');
+        }
     }
-
-    public function create()
-    {
-        $categories = category::all();
-        return view('item.create', compact('categories'));
-    }
-
-    public function store(Request $request)
-    {
-        $this->validate($request, [
-            'item_name' =>'required',
-            'category_id' =>'required',
-        ]);
-
-        item::create([
-            'item_name' =>$request->item_name,
-            'category_id'=>$request->category_id,
-        ]);
-        return redirect()->route('item.index')->with('success', 'Created successfully!' );
-    }
-
-    public function edit($id)
-    {
-        $items = item::find($id);
-        return view('item.edit', compact('items'));
-    }
-
-    public function update(Request $request, $id)
-    {
-        $this->validate($request, [
-            'item_name' =>'required'
-        ]);
-
-        item::where('id', $id)->update([
-            'item_name' =>$request->item_name,
-        ]);
-        return redirect()->route('item.index', $id)->with('success', 'Edited successfully!' );
-    }
-
-    public function delete($id)
-    {
-        item::where('id', $id)->delete();
-        return redirect()->route('item.index')->with('success', 'Deleted successfully');
-    }
-}
+?>
